846562a5bc07d5aa7c334a23b6922b67e52ab13c
BlockListed <44610569+BlockListed@users.noreply.github.com>
2022-02-19 17:54:50 +0100

    fixed logging, correct file is now displayed

--- discord/discord.go
+++ discord/discord.go
@@ -6,10 +6,10 @@ import (
 	"nathanman/database"
 	"nathanman/model"
 	"nathanman/regex"
-	"nathanman/util"
 	"sync"
 	"time"
 
+	"github.com/TwiN/go-color"
 	"github.com/bwmarrin/discordgo"
 	"github.com/jinzhu/now"
 )
@@ -23,7 +23,7 @@ func Run(quit chan interface{}, wg *sync.WaitGroup) {
 
 	dg, err := discordgo.New("Bot " + config.Configuration.Discord.Token)
 	if err != nil {
-		util.PanicInRed(fmt.Sprintf("Bot couldn't be started! Token: %s. %s", config.Configuration.Discord.Token, err))
+		config.Logger.Panicln(color.InRed(fmt.Sprintf("Bot couldn't be started! Token: %s. %s", config.Configuration.Discord.Token, err)))
 	}
 	defer wg.Done()
 	defer dg.Close()
@@ -32,7 +32,7 @@ func Run(quit chan interface{}, wg *sync.WaitGroup) {
 	dg.AddHandler(messageCreate)
 
 	if err = dg.Open(); err != nil {
-		util.PanicInRed(fmt.Sprintf("Couldn't start bot! %v", err))
+		config.Logger.Panicln(color.InRed(fmt.Sprintf("Couldn't start bot! %v", err)))
 	}
 	go poll_rename(dg)
 
@@ -47,7 +47,7 @@ func poll_rename(s *discordgo.Session) {
 	for {
 		time.Sleep(duration)
 		if err := database.Db.Where("bot = ?", "nathanman").Take(&config_data).Error; err != nil {
-			util.LogInYellow(fmt.Sprintf("Couldn't get configuration for bot. %v", err))
+			config.Logger.Println(color.InYellow(fmt.Sprintf("Couldn't get configuration for bot. %v", err)))
 			continue
 		}
 		nextDayLastime := now.With(config_data.Lasttime).BeginningOfDay().AddDate(0, 0, 1)
@@ -61,19 +61,19 @@ func poll_rename(s *discordgo.Session) {
 
 			setUsername(s)
 			if err := s.GuildMemberNickname(config.Configuration.Discord.GuildId, config.Configuration.Discord.UserId, entry.Name); err != nil {
-				util.LogInYellow(fmt.Sprintf("Couldn't rename member %v to %v. %v", userName, entry.Name, err))
+				config.Logger.Println(color.InYellow(fmt.Sprintf("Couldn't rename member %v to %v. %v", userName, entry.Name, err)))
 				return
 			} else {
 				config.Logger.Printf("Renamed user %s to %s! \n", userName, entry.Name)
 			}
 			if err := database.Db.Where("name = ?", entry.Name).Delete(&model.Entry{}).Error; err != nil {
-				util.LogInYellow(fmt.Sprintf("Couldnt delete entry: %v. %v", entry, err))
+				config.Logger.Println(color.InYellow(fmt.Sprintf("Couldnt delete entry: %v. %v", entry, err)))
 				return
 			}
 
 			config_data.Lasttime = now.BeginningOfDay()
 			if err := database.Db.Where("bot = ?", "nathanman").Save(config_data).Error; err != nil {
-				util.LogInYellow(fmt.Sprintf("Couldn't save configuration: %v. %v", config_data, err))
+				config.Logger.Println(color.InYellow(fmt.Sprintf("Couldn't save configuration: %v. %v", config_data, err)))
 			}
 		}
 	}
@@ -83,9 +83,9 @@ func setUsername(s *discordgo.Session) {
 	tmp, usererror := s.User(config.Configuration.Discord.UserId)
 	tmp2, usernickerror := s.GuildMember(config.Configuration.Discord.GuildId, config.Configuration.Discord.UserId)
 	if usererror != nil {
-		util.PanicInRed(fmt.Sprintf("UserId %s wasn't valid!, %s \n", config.Configuration.Discord.UserId, usererror))
+		config.Logger.Panicln(color.InRed(fmt.Sprintf("UserId %s wasn't valid!, %s \n", config.Configuration.Discord.UserId, usererror)))
 	} else if usernickerror != nil {
-		util.PanicInRed(fmt.Sprintf("UserId %s or GuildId %s wasn't valid!, %s \n", config.Configuration.Discord.UserId, config.Configuration.Discord.GuildId, usernickerror))
+		config.Logger.Panicln(color.InRed(fmt.Sprintf("UserId %s or GuildId %s wasn't valid!, %s \n", config.Configuration.Discord.UserId, config.Configuration.Discord.GuildId, usernickerror)))
 	}
 	if tmp2.Nick != "" {
 		userName = tmp2.Nick
@@ -97,7 +97,7 @@ func setUsername(s *discordgo.Session) {
 
 func ready(s *discordgo.Session, event *discordgo.Ready) {
 	if err := s.UpdateGameStatus(0, "Jonathan befreunden!"); err != nil {
-		util.LogInYellow(fmt.Sprintf("Couldn't set game status. %v", err.Error()))
+		config.Logger.Println(color.InYellow(fmt.Sprintf("Couldn't set game status. %v", err.Error())))
 	}
 	setUsername(s)
 	config.Logger.Printf("Bot started as %s.", s.State.User.Username)
@@ -112,9 +112,9 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			name := mp[0]
 			if len(name) > 32 || regex.BadwordRegex.MatchString(name) {
 				if _, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Nichtvalider nutzername: %v. %v", name, m.Author.Mention())); err != nil {
-					util.LogInYellow(fmt.Sprintf("Couldn't send plaintext message. %v", err))
+					config.Logger.Println(color.InYellow(fmt.Sprintf("Couldn't send plaintext message. %v", err)))
 				}
-				util.LogInYellow(fmt.Sprintf("Invalid name: %v", name))
+				config.Logger.Println(color.InYellow(fmt.Sprintf("Invalid name: %v", name)))
 				return
 			}
 
@@ -135,7 +135,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					},
 				})
 				if err != nil {
-					util.LogInYellow(fmt.Sprintf("Couldn't send embed message: %v. %v", msg, err))
+					config.Logger.Println(color.InYellow(fmt.Sprintf("Couldn't send embed message: %v. %v", msg, err)))
 				}
 			}
 		}
