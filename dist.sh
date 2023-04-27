dbee2fe21c5f68d96eb63b78542a134032136038
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-04-27 21:27:36 +0200

    use gitea as registry

--- dist.sh
+++ dist.sh
@@ -3,5 +3,5 @@ PATH=/usr/bin
 
 mkdir -p dist
 
-docker build -t registry.box/nathanman:latest .
-docker push registry.box/nathanman:latest
+docker build -t gitea.box/aaron/nathanman:latest .
+docker push gitea.box/aaron/nathanman:latest
